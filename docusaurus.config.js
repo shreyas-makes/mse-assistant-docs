03ae47855eec92d66d837f385b4d8a6af6b4053a
Shreyas <shreyas314159@gmail.com>
2024-02-27 21:19:52 +0000

    base url change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://shreyas314159.github.io/mse-assistant-docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/mse-assistant-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
